e347abc2567acc6f4651ac6e791af34c7bffdc92
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2024-09-29 21:05:20 -0500

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+// Task 1: Reverse a String
+// Write a function that takes a string and returns it reversed.
+function reverseString(str){
+  return  reversedString = str.split("").reverse().join("");
+}
+
+console.log(reverseString("Hello"))
